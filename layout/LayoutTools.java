255213109e4f11be0438843513a1ab816e7fac2d
dexter <dexterwork2010@gmail.com>
2015-07-23 23:21:40 +0800

    update LayoutTools

--- layout/LayoutTools.java
+++ layout/LayoutTools.java
@@ -4,6 +4,9 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewTreeObserver;
+
+import java.util.HashMap;
 
 import studio.dexter.ohmoney.R;
 
@@ -12,6 +15,7 @@ import studio.dexter.ohmoney.R;
  */
 public class LayoutTools {
 
+
     public static LayoutInflater getInflater(Context context) {
         return (LayoutInflater) context
                 .getSystemService(context.LAYOUT_INFLATER_SERVICE);
@@ -19,14 +23,49 @@ public class LayoutTools {
 
     public static AlertDialog.Builder getCustomAlertDialog(Context ctx, int customLayout) {
         LayoutInflater inflater = LayoutTools.getInflater(ctx);
-        View view=inflater.inflate(customLayout,null);
-        return getCustomAlertDialog(ctx,view);
+        View view = inflater.inflate(customLayout, null);
+        return getCustomAlertDialog(ctx, view);
     }
-    public static AlertDialog.Builder getCustomAlertDialog(Context ctx,View view){
+
+    public static AlertDialog.Builder getCustomAlertDialog(Context ctx, View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
         builder.setView(view);
         builder.create();
         return builder;
+    }
+
+    public static HashMap<LayoutFlag, Integer> getLayoutSize(final View view) {
+        final HashMap<LayoutFlag, Integer> map = new HashMap<LayoutFlag, Integer>();
+        ViewTreeObserver obs = view.getViewTreeObserver();
+        obs.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                map.put(LayoutFlag.HEIGHT, view.getHeight());
+                map.put(LayoutFlag.WIDTH, view.getWidth());
+                view.getViewTreeObserver().removeGlobalOnLayoutListener(
+                        this);
+            }
+        });
+        return map;
+    }
+
+    public static HashMap<LayoutFlag, Float> getLayoutPosition(final View view) {
+        final HashMap<LayoutFlag, Float> map = new HashMap<LayoutFlag, Float>();
+        ViewTreeObserver obs = view.getViewTreeObserver();
+        obs.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                map.put(LayoutFlag.X, view.getX());
+                map.put(LayoutFlag.Y, view.getY());
+                view.getViewTreeObserver().removeGlobalOnLayoutListener(
+                        this);
+            }
+        });
+        return map;
+    }
+
+    public enum LayoutFlag {
+        WIDTH, HEIGHT, X, Y
     }
 
 }
