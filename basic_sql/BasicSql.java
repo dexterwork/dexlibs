c64262013505246bfa367669a675f8a084dd0323
dexter <dexterwork2010@gmail.com>
2015-07-26 20:51:28 +0800

    update basic database

--- basic_sql/BasicSql.java
+++ basic_sql/BasicSql.java
@@ -1,26 +1,27 @@
-package studio.dexter.basic;
+package studio.dexter.basic_sql;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.TreeMap;
+
+import studio.dexter.basic_sql.SqlParser;
 
 /**
  * Created by Dexter on 2015/7/25.
  */
 public class BasicSql extends SQLiteOpenHelper {
-    protected HashMap<String, HashMap<String, String>> tables;
+    protected TreeMap<String, TreeMap<String, String>> tables;
     protected Context context;
 
-    public BasicSql(Context context, String dbName, ArrayList<String> tableFilesName, int version) {
+    public BasicSql(Context context, String dbName, String[] tableFilesName, int version) {
         super(context, dbName, null, version);
         this.context = context;
         tables = getTableMaps(context, tableFilesName);
     }
 
-    private HashMap<String, HashMap<String, String>> getTableMaps(Context ctx, ArrayList<String> tableFileName) {
+    private TreeMap<String, TreeMap<String, String>> getTableMaps(Context ctx, String[] tableFileName) {
         SqlParser sqlParser = new SqlParser();
         return sqlParser.getTables(ctx, tableFileName);
     }
@@ -28,7 +29,7 @@ public class BasicSql extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         SqlParser sqlParser = new SqlParser();
-        for (HashMap<String, String> map : tables.values())
+        for (TreeMap<String, String> map : tables.values())
             db.execSQL(sqlParser.parserCreateTableExecToString(map));
     }
 
--- basic_sql/SqlParser.java
+++ basic_sql/SqlParser.java
@@ -1,12 +1,12 @@
-package studio.dexter.basic;
+package studio.dexter.basic_sql;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.TreeMap;
 
 import studio.dexter.container.Keys;
 import studio.dexter.tools.MLog;
@@ -21,22 +21,22 @@ public class SqlParser {
      * @param filename the file in assets folder.
      * @return HashMap(TableName, FieldName and Typed)
      */
-    public HashMap<String, HashMap<String, String>> getTables(Context context, ArrayList<String> filename) {
-        HashMap<String, HashMap<String, String>> tables = new HashMap<String, HashMap<String, String>>();
+    public TreeMap<String, TreeMap<String, String>> getTables(Context context, String[] filename) {
+        TreeMap<String, TreeMap<String, String>> tables = new TreeMap<String, TreeMap<String, String>>();
         for (String name : filename) {
-            HashMap<String, String> map = parserFileFromAssets(context, name);
+            TreeMap<String, String> map = parserFileFromAssets(context, name);
             tables.put(map.get(Keys.TABLE_NAME), map);
         }
         return tables;
     }
 
 
-    private HashMap<String, String> parserFileFromAssets(Context context, String filename) {
+    private TreeMap<String, String> parserFileFromAssets(Context context, String filename) {
 //        file format:
 //        xxx,xxx
 //        xxx, xxx
 //        ...
-        HashMap<String, String> map = new HashMap<String, String>();
+        TreeMap<String, String> map = new TreeMap<String, String>();
         InputStream input = null;
         boolean showLog = true;
         try {
@@ -51,12 +51,16 @@ public class SqlParser {
 
             String[] temp = null;
             for (String str : array) {
+                if (TextUtils.isEmpty(str)) continue;
                 temp = str.split(",");
                 temp[0] = temp[0].trim();
+                temp[1] = filterNote(temp[1]);
                 temp[1] = temp[1].trim();
                 temp[1] = toUpperWithCheck(temp[0], temp[1]);
                 map.put(temp[0], temp[1]);
             }
+
+            map = addReservedFields(map);
 
             if (showLog)
                 for (String str : map.keySet()) {
@@ -68,17 +72,40 @@ public class SqlParser {
         return map;
     }
 
+    private String filterNote(String str) {
+        if (!str.contains("/")) return str;
+        return str.substring(0, str.indexOf("/") - 1);
+    }
+
+    /**
+     * 增加保留欄位
+     *
+     * @param map
+     * @return
+     */
+    private TreeMap<String, String> addReservedFields(TreeMap<String, String> map) throws RuntimeException {
+        if (!map.keySet().contains(Keys.EMPTY)) return map;
+        int addQuantity = Integer.valueOf(map.get(Keys.EMPTY).trim());
+        String field = "field";
+        if (map.keySet().contains(Keys.FIELD)) field = map.get(Keys.FIELD);
+        for (int i = 0; i < addQuantity; i++) {
+            map.put(field.toLowerCase() + "_" + i, "TEXT");
+        }
+        return map;
+    }
+
+
     /**
      * \
      *
      * @param tableMap
      * @return database execute create table command string.
      */
-    public String parserCreateTableExecToString(HashMap<String, String> tableMap) {
+    public String parserCreateTableExecToString(TreeMap<String, String> tableMap) {
         if (tableMap == null)
-            throw new RuntimeException("non table map:BasicSql.getNewTableField()");
+            throw new RuntimeException("non table map:BasicSql.getNewTableField() /or key/value is error.");
         if (!tableMap.containsKey(Keys.TABLE_NAME))
-            throw new RuntimeException("non table name:BasicSql.getNewTableField()");
+            throw new RuntimeException("non table name:BasicSql.getNewTableField() /or key/value is error.");
         String tableName = tableMap.get(Keys.TABLE_NAME);
         if (tableMap.containsKey(Keys._ID)) tableMap.remove(Keys._ID);
         String createTable = "create table " + tableName + "(_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,";
@@ -87,7 +114,7 @@ public class SqlParser {
         String key = "";
         while (it.hasNext()) {
             key = (String) it.next();
-            if (key.equals(Keys.TABLE_NAME) || key.equals(Keys.DB)) continue;
+            if (dontAddMapCheck(key)) continue;
             createTable += key + " " + tableMap.get(key);
             if (it.hasNext()) createTable += ",";
         }
@@ -104,6 +131,17 @@ public class SqlParser {
         if (key.equals(Keys._ID)) return str;
         if (key.equals(Keys.DB)) return str;
         if (key.equals(Keys.TABLE_NAME)) return str;
+        if (key.equals(Keys.EMPTY)) return str;
+        if (key.equals(Keys.FIELD)) return str;
         return str.toUpperCase();
+    }
+
+    private boolean dontAddMapCheck(String key) {
+        if (key.equals(Keys._ID)) return true;
+        if (key.equals(Keys.TABLE_NAME)) return true;
+        if (key.equals(Keys.DB)) return true;
+        if (key.equals(Keys.EMPTY)) return true;
+        if (key.equals(Keys.FIELD)) return true;
+        return false;
     }
 }
