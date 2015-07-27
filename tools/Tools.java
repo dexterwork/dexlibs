a621ac137adb211a7ef74f2a8bae35b81d3d2315
dexter <dexter@a-want.com>
2015-07-27 17:52:57 +0800

    update

--- tools/Tools.java
+++ tools/Tools.java
@@ -1,5 +1,7 @@
 package com.awant.lion.tools;
 
+import android.content.Context;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +11,12 @@ import java.io.InputStreamReader;
  * Created by dexter on 2015/6/17.
  */
 public class Tools {
+
+
+   public static String getDeviceId(Context context) {
+      return android.provider.Settings.Secure.getString(context.getContentResolver(),
+                android.provider.Settings.Secure.ANDROID_ID);
+    }
 
     public static String strLen(String str, int len) {
         while (str.length() < len) str = "0" + str;
