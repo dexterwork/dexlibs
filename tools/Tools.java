f7e4336ab697623028807ef970f0fac704b8e755
dexter <dexter@a-want.com>
2015-09-05 13:22:36 +0800

    add clear cache

--- tools/Tools.java
+++ tools/Tools.java
@@ -60,4 +60,28 @@ public class Tools {
         double d = Double.valueOf(str);
         return String.valueOf( (int) d);
     }
+
+
+    public void clearCache(Context context) {
+        try {
+            File dir = context.getCacheDir();
+            if (dir != null && dir.isDirectory()) deleteDir(dir);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
 }
