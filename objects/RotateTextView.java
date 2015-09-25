d6c0de4e52b7b5f269a5852151964d20801d21de
dexter <dexter@a-want.com>
2015-09-25 11:04:32 +0800

    RotateTextView

---
+++ objects/RotateTextView.java
@@ -0,0 +1,62 @@
+package com.awant.lion.objects;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import com.awant.lion.R;
+
+/**
+ * Created by dexter on 2015/9/25.
+ */
+public class RotateTextView extends TextView {
+
+    private static final int DEFAULT_DEGREES = 0;
+    private int mDegrees;
+
+
+    public RotateTextView(Context context) {
+        super(context);
+    }
+
+    public RotateTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public RotateTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        this.setGravity(Gravity.CENTER);
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.RotateTextView);
+        mDegrees = a.getDimensionPixelSize(R.styleable.RotateTextView_degree,
+                DEFAULT_DEGREES);
+        a.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        canvas.translate(getCompoundPaddingLeft(), getExtendedPaddingTop());
+        canvas.rotate(mDegrees, this.getWidth() / 2f, this.getHeight() / 2f);
+        super.onDraw(canvas);
+        canvas.restore();
+    }
+
+    public void setDegrees(int degrees) {
+        mDegrees = degrees;
+    }
+}
