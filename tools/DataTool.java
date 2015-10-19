2364af4d0ce2a841ab36fb7d8403fdb09e497652
dexter <dexter@a-want.com>
2015-10-19 11:22:46 +0800

    data tool

---
+++ tools/DataTool.java
@@ -0,0 +1,21 @@
+﻿package com.awant.lion.tools;
+
+import android.database.Cursor;
+
+import com.awant.lion.data_provider.AppDataContract;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+/**
+ * Created by dexter on 2015/10/2.
+ */
+public class DataTool {
+    
+
+
+     public static boolean matchesEmail(String email) {
+        return email.matches("^[_a-z0-9-]+([.][_a-z0-9-]+)*@[a-z0-9-]+([.][a-z0-9-]+)*$") ? true : false;
+    }
+}
