5a5e5996835bd2421fb4d5b0a6481d2c28fa5ce8
dexter <dexter@a-want.com>
2015-11-04 14:41:58 +0800

    SquareButton

---
+++ objects/SquareButton.java
@@ -0,0 +1,31 @@
+package com.awant.lion.objects;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by dexter on 2015/11/4.
+ */
+public class SquareButton extends Button {
+    public SquareButton(Context context) {
+        super(context);
+    }
+
+    public SquareButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(getDefaultSize(0, widthMeasureSpec), getDefaultSize(0, heightMeasureSpec));
+        int childWidthSize = getMeasuredWidth();
+        heightMeasureSpec = widthMeasureSpec = MeasureSpec.makeMeasureSpec(childWidthSize, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+}
