0108b791883317c19b2f03c0da8dd1966a6c0615
dexter <dexter@skyking.com.tw>
2016-08-15 09:48:44 +0800

    add permission 6

---
+++ permission/permission/PermissionsChecker.java
@@ -0,0 +1,32 @@
+package permission;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by SkykingAndroid on 2016/8/9.
+ */
+public class PermissionsChecker {
+    Context context;
+
+    public PermissionsChecker(Context context) {
+        this.context = context;
+    }
+
+
+    // 判断权限集合
+    public boolean lacksPermissions(String... permissions) {
+        for (String permission : permissions) {
+            if (lacksPermission(permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 判断是否缺少权限
+    private boolean lacksPermission(String permission) {
+        return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED;
+    }
+}
