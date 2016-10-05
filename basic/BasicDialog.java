5066a56d75e981280c374d81142bdaec9935d6c6
dexter <dexter@skyking.com.tw>
2016-10-05 11:11:38 +0800

    add basic dialog

---
+++ basic/BasicDialog.java
@@ -0,0 +1,48 @@
+package com.twgood.skytv_test.basic;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
+
+/**
+ * Created by Dexter on 2016/10/5.
+ */
+
+public abstract class BasicDialog extends Dialog {
+    protected int dialogId;
+    protected BasicActivity activity;
+
+    public BasicDialog(BasicActivity activity) {
+        super(activity);
+        this.activity = activity;
+        init();
+        setContentView(getCustomLayoutResId());
+    }
+
+    public BasicDialog(BasicActivity activity, int dialogId) {
+        this(activity);
+        this.dialogId = dialogId;
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        initLayout();
+    }
+
+    protected abstract void initLayout();
+
+    protected abstract int getCustomLayoutResId();
+
+
+    private void init() {
+        setCanceledOnTouchOutside(false);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+    public int getDialogId() {
+        return dialogId;
+    }
+}
