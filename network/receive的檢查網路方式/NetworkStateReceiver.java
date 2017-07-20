fe3534e5ffce4ec991f0bf14869dcfd2305d8013
dexter <dexter@skyking.com.tw>
2017-07-20 14:44:20 +0800

    update NetworkReceiver

--- network/receive的檢查網路方式/NetworkStateReceiver.java
+++ network/receive的檢查網路方式/NetworkStateReceiver.java
@@ -1,8 +1,9 @@
-package receiver;
+package com.skyking.skykingradio;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
@@ -14,30 +15,38 @@ public class NetworkStateReceiver extends BroadcastReceiver {
 
     NetworkListener networkListener;
 
+    public static final String RECEIVER_NAME = "android.net.conn.CONNECTIVITY_CHANGE";
+
+
     public NetworkStateReceiver(NetworkListener networkListener) {
         this.networkListener = networkListener;
     }
+
+    public void register(Context context) {
+        context.registerReceiver(this, new IntentFilter(RECEIVER_NAME));
+    }
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
 //        MLog.d("NetworkStateReceiver","Network connectivity change");
         if (intent.getExtras() != null) {
             NetworkInfo ni = (NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
-            networkListener.onNetworkChange(ni);
+            networkListener.onNetworkChange(ni, ni != null && ni.getState() == NetworkInfo.State.CONNECTED);
 
 
-            if (ni != null && ni.getState() == NetworkInfo.State.CONNECTED) {
-//                MLog.i("NetworkStateReceiver", "Network " + ni.getTypeName() + " connected");
-
-            }
+//            if (ni != null && ni.getState() == NetworkInfo.State.CONNECTED) {
+////                MLog.i("NetworkStateReceiver", "Network " + ni.getTypeName() + " connected");
+//
+//            }
         }
         if (intent.getExtras().getBoolean(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
 //            MLog.d("NetworkStateReceiver", "There's no network connectivity");
-            networkListener.onNetworkChange(null);
+            networkListener.onNetworkChange(null, false);
         }
     }
 
     public interface NetworkListener {
-        void onNetworkChange(NetworkInfo networkInfo);
+        void onNetworkChange(NetworkInfo networkInfo, boolean isConnect);
     }
 }
