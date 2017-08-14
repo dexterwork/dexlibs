7788b09c376a1f02d623dd99f3bf61f7de045345
dexter <dexter@skyking.com.tw>
2017-08-14 12:03:08 +0800

    update permission

--- permission/permission_v3/PermissionReceiver.java
+++ permission/permission_v3/PermissionReceiver.java
@@ -1,9 +1,17 @@
-package dexterliu.studio.dexlibs.tools.permission;
+package com.skyking.skylib.permission_v3;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Dexter on 2017/8/13.
@@ -13,7 +21,7 @@ public abstract class PermissionReceiver extends BroadcastReceiver {
     public static final String RECEIVER_NAME = PermissionReceiver.class.getSimpleName();
     public static final String KEY = "KEY";
 
-    public void register(Context context) {
+    private void register(Context context) {
         context.registerReceiver(this, new IntentFilter(RECEIVER_NAME));
     }
 
@@ -21,9 +29,24 @@ public abstract class PermissionReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent == null) return;
         boolean pass = intent.getBooleanExtra(KEY, false);
+        context.unregisterReceiver(this);
         if (pass) onPass();
         else onDeny();
-        context.unregisterReceiver(this);
+    }
+
+
+    public void check(Activity activity, String permission) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            onPass();
+            return;
+        }
+        int per = ContextCompat.checkSelfPermission(activity, permission);
+        if (per == PackageManager.PERMISSION_GRANTED) {
+            onPass();
+            return;
+        }
+        register(activity);
+        ActivityCompat.requestPermissions(activity, new String[]{permission}, 0);
     }
 
     public abstract void onPass();
